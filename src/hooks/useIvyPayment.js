fc72073621b7030f1af84925daa76a3bc8b8e1f6
Kostiantyn Sprohe <kostya.sproge@gmail.com>
2022-12-17 03:45:47 +0200

    fixed eslint

--- src/hooks/useIvyPayment.js
+++ src/hooks/useIvyPayment.js
@@ -1,24 +1,21 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 
-import { __ } from '../../../../i18n';
 import createIvyCheckoutSession from '../api/cart/createIvyCheckoutSession';
 import useIvyPaymentAppContext from "./useIvyPaymentAppContext";
 
 export default function useIvyPayment() {
     const {setPageLoader, appDispatch} = useIvyPaymentAppContext();
-    const [sessionConfig, setSessionConfig] = useState(null);
 
     const placeOrder = useCallback(async () => {
         try {
             setPageLoader(true);
             await createIvyCheckoutSession(appDispatch, false);
-            setIsInitialized(true);
         } catch (error) {
             console.error(error);
         } finally {
             setPageLoader(false);
         }
-    }, [setPageLoader, isInitialized, getCheckoutSessionConfig]);
+    }, [setPageLoader, createIvyCheckoutSession]);
 
     return {
         placeOrder
