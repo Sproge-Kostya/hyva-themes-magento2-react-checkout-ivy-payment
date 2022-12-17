2dcdc748b3ee3b94a6c2d09ea82932c646b4a09d
Kostiantyn Sprohe <kostya.sproge@gmail.com>
2022-12-17 03:26:32 +0200

    fixed default import mutation

--- src/api/cart/createIvyCheckoutSession/createIvyCheckoutSession.js
+++ src/api/cart/createIvyCheckoutSession/createIvyCheckoutSession.js
@@ -1,7 +1,7 @@
 import LocalStorage from "../../../../../../utils/localStorage";
 import modifier from "./modifier";
 import sendRequest from "../../../../../../api/sendRequest";
-import CRETE_IVY_CHECKOUT_SESSION from "./mutation";
+import { CRETE_IVY_CHECKOUT_SESSION } from "./mutation";
 
 export default async function createIvyCheckoutSession(dispatch, express = true) {
     const variables = { cartId: LocalStorage.getCartId(), express: express };
